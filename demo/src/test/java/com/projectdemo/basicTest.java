faf9eace980b9907f5fcf4b7bd66f60f3a233e54
Nexecis <nexecis@gmail.com>
2023-03-30 21:07:54 +0800

    setup for test environment

---
+++ demo/src/test/java/com/projectdemo/basicTest.java
@@ -0,0 +1,13 @@
+package com.projectdemo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class basicTest {
+    @Test
+    public void Unit_Test_1(){
+        assertEquals('a', 'a');
+    }
+
+}
